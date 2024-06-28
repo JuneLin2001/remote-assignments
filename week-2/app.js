9c202d7833758fb15df5c3320bf18df61d9bd174
JuneLin2001 <asd900606@gmail.com>
2024-06-28 13:39:57 +0800

    add 1 2 3

--- week-2/app.js
+++ week-2/app.js
@@ -4,7 +4,22 @@ WelcomeWord.addEventListener('click', () => {
     WelcomeWord.innerHTML = 'Have a Good Time!';
 });
 
+// Request 2: Click to Show/Close Menu.
+const menuTrigger = document.querySelector('#menu-trigger');
+const menu = document.querySelector('.menu');
+const menuClose = document.querySelector('#menu-close');
 
+menuTrigger.addEventListener('click', () => {
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+});
+
+menuClose.addEventListener('click', () => {
+    menu.style.display = 'none';
+});
 
 // Request 3: Click to Show More Content Boxes.
 const callToAction = document.querySelector('.CallToAction');
@@ -15,9 +30,4 @@ callToAction.addEventListener('click', () => {
     else {
         document.querySelector('.display-none').style.display = 'none';
     }
-})
-
-// Request 2: Click to Show/Close Menu.
-const MenuTrigger = document.querySelector('menu-trigger');
-MenuTrigger.addEventListener('click', () => {
 })
