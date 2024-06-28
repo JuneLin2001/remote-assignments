fd3fb1e4102ac1847d47bd45caad3aeff50ffd6b
JuneLin2001 <asd900606@gmail.com>
2024-06-28 12:31:34 +0800

    completed Request 1&3

--- week-2/app.js
+++ week-2/app.js
@@ -1,1 +1,23 @@
-alert('hello')
+// Request 1: Click to Change Text.
+const WelcomeWord = document.getElementById('WelcomeMessage');
+WelcomeWord.addEventListener('click', () => {
+    WelcomeWord.innerHTML = 'Have a Good Time!';
+});
+
+
+
+// Request 3: Click to Show More Content Boxes.
+const callToAction = document.querySelector('.CallToAction');
+callToAction.addEventListener('click', () => {
+    if (document.querySelector('.display-none').style.display === 'none') {
+        document.querySelector('.display-none').style.display = 'flex';
+    }
+    else {
+        document.querySelector('.display-none').style.display = 'none';
+    }
+})
+
+// Request 2: Click to Show/Close Menu.
+const MenuTrigger = document.querySelector('menu-trigger');
+MenuTrigger.addEventListener('click', () => {
+})
