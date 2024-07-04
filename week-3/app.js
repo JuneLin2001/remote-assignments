4332138c3bcdf3d579ff5549d7ba0cd7a026b3c5
JuneLin <asd900606@gmail.com>
2024-07-04 10:42:18 +0800

    prettier

--- week-3/app.js
+++ week-3/app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
