9465d60d99cfb075ed0c2f1d54a005eb57d06bb9
JuneLin <asd900606@gmail.com>
2024-07-04 10:28:29 +0800

    add localhost:3000

--- week-3/app.js
+++ week-3/app.js
@@ -5,6 +5,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
