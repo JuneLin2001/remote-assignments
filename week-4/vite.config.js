d35d90496d701b34e46fed04d8546a5b89435213
JuneLin <asd900606@gmail.com>
2024-07-09 15:04:09 +0800

    adjust base

--- week-4/vite.config.js
+++ week-4/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  base:'https://junelin2001.github.io/remote-assignments/'
+  base:'/<remote-assignments>/'
 })
