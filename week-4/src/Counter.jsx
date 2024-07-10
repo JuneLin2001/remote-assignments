85624d91ac6e358e0aadcadae052cfe687f58ffb
JuneLin <asd900606@gmail.com>
2024-07-10 16:19:39 +0800

    try Assignment-5

--- week-4/src/Counter.jsx
+++ week-4/src/Counter.jsx
@@ -4,12 +4,22 @@ const Counter = () => {
   return <button onClick={console.log("BTN is on click")}>+1</button>;
 };
 
+const AllPlus1 = () => {
+  return <button onClick={console.log("BTN is on click")}>+1</button>;
+};
+
+const AddCounter = () => {
+  return <button onClick={console.log("BTN is on click")}>+1</button>;
+};
+
 const Wireframe = () => {
   return (
     <>
+      <AllPlus1 />
       <Counter />
       <Counter />
       <Counter />
+      <AddCounter />
     </>
   );
 };
